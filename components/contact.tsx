cb68d8e614b7cf753fea3eef8e5edd8879044122
Punarv Pawade <Punarvpawade17@gmail.com>
2024-08-15 01:24:43 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          punarvpawadde17@gmail.com
+        <a className="underline" href="mailto:punarvpawade17@gmail.com">
+          punarvpawade17@gmail.com
         </a>{" "}
         or through this form.
       </p>
